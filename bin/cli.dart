075a76aa54cd14a381a43df4d70776c5a7cf3362
CapableCCat <Z2481331250@163.com>
2026-04-02 22:08:04 +0800

    feat: add CLI interactivity support
    为应用添加基本交互能力：
    - 支持用户输入并进行处理
    - 提供命令行使用提示
    - 完善函数调用与空安全约束
    
    Add basic CLI interactivity support:
    - Handle user input and processing
    - Provide usage instructions
    - Improve function structure and null safety
    
    2026.04.02

--- bin/cli.dart
+++ bin/cli.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 // const version = '0.0.1'; // Add this line
 
 // void main(List<String> arguments) {
@@ -11,17 +13,20 @@
 const version = '0.1.0';
 
 void main(List<String> arguments) {
-  if (arguments.isEmpty) {
-    print('Hello, World!');
-    print('Hello, Dart!');
+  if (arguments.isEmpty || arguments.first == 'help') {
+    printUsage();
   }
-  // else if (arguments.first == 'version') {
-  //   print('Dartpedia CLI version $version');
+  // if (arguments.isEmpty) {
+  //   print('Hello, World!');
+  //   print('Hello, Dart!');
   // }
-  else if (arguments.first == 'search') {
+  else if (arguments.first == 'version') {
+    print('Dartpedia CLI version $version');
+  } else if (arguments.first == 'search') {
     print('Search command recognized!');
-  } 
-  else {
+    final inputArgs = arguments.length > 1 ? arguments.sublist(1) : null;
+    searchWikipedia(inputArgs);
+  } else {
     printUsage();
   }
 }
@@ -30,4 +35,32 @@ void printUsage() {
   print(
     "The following commmands are valid: 'help', 'version', 'search <ARTICLE-TITLE>'",
   );
+}
+
+void searchWikipedia(List<String>? arguments) {
+  // print('SearchingWikipedia received arguments: $arguments');
+  final String articleTitle;
+
+  // 如果用户没有传入参数,则提示用户输入文章标题
+  // If the user didn't pass in arguments, request an article title.
+  if (arguments == null || arguments.isEmpty) {
+    // Dart 静态分析可以检测到 articleTitle 在执行 print 语句时保证被初始化。无论该函数体中路径如何，该变量都是不可空的。
+    // Dart static analysis can detect that articleTitle is guaranteed to be initialized when the print statement is executed.
+    // No matter which path is taken through this function body, the variable is non-nullable.
+    print('Please provide an article title.');
+
+    // 等待输入,如果输入为空,则返回空字符串作为默认值
+    // Await input and provide a default empty string if the input is null.
+    articleTitle = stdin.readLineSync() ?? '';
+  } else {
+    // 否则,将参数合并为单个字符串
+    // Otherwise, join the arguments into a single string.
+    articleTitle = arguments.join(' ');
+  }
+
+  // print('Current article title: $articleTitle');
+
+  print('Looking up article about "$articleTitle". Please wait...');
+  print('Here ya go!');
+  print('(Pretend this is an article about "$articleTitle" from Wikipedia.)');
 }
